83e33ab88e35d47aabda169050d27fc71a034000
imraf <refaelba@gmail.com>
2020-04-26 14:16:07 +0300

    changed file name.

--- webapp/run.py
+++ webapp/run.py
@@ -1,3 +1,3 @@
-from webapp.taskany.taskany import app
+from webapp.taskany.app import app
 
 app.run(host="0.0.0.0", port=5000)
--- webapp/taskany/models/models.py
+++ webapp/taskany/models/models.py
@@ -7,7 +7,7 @@ from mongoengine import StringField, BooleanField, ListField, DateTimeField, Ref
 from enum import Enum, auto
 import datetime
 from bson import ObjectId
-from webapp.taskany.taskany import login
+from webapp.taskany.app import login
 
 
 class Role(Enum):
